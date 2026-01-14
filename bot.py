f6d17f56d0962cf92dbd213548d80af164bc8cff
Arthur Badretdinov <artur.badretdinov11@gmail.com>
2026-01-14 11:26:35 +0500

    add help command handler

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
+from handlers import help
 
 
 async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    
+    dp.include_router(help.router)
+    
     await dp.start_polling(bot)
 
 
